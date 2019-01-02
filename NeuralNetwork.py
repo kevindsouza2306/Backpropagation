69f28b26e45e7c729657faf8c7f3fc01f049ac5d
Administrator <kevindsouza2306@gmail.com>
2019-01-02 19:32:26 +0530

    nn_XOR.py added

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -9,7 +9,7 @@ class NeuralNetwork:
         self.alpha = alpha
 
         for i in np.arange(0, len(layers) - 2):
-            w = np.random.randn(layers[i])
+            w = np.random.randn(layers[i]+1, layers[i+1]+1)
             self.W.append(w / np.sqrt(layers[i]))
 
         w = np.random.randn(layers[-2] + 1, layers[-1])
